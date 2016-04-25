fb627c699469a15e18e3a23cb2a4e2257bf551cf
pthoang <vicentethien@hotmail.com>
2016-04-25 16:53:34 +0200

    make it better

--- app/webapp/webapp.js
+++ app/webapp/webapp.js
@@ -21,7 +21,7 @@ angular.module('mainApp.webapp',['ngRoute', 'ngCookies'])
                 $scope.loading = false;
             })
             .error(function(response) {
-                alert("En feil oppstod, pr�v igjen om 1 minutt")
+                alert("En feil oppstod, prøv igjen om 1 minutt")
             });
 
         $scope.openSuggestion = function() {
